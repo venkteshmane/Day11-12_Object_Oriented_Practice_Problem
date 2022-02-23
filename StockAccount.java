a02dd1dc003620047af980aaf7c0de893faf8024
venktesh.mane18@gmail.com <venktesh.mane18@gmail.com>
2022-02-23 17:12:26 +0530

    OOps problems

---
+++ StockAccount.java
@@ -0,0 +1,40 @@
+package objectOrientedPrograms;
+
+public class StockAccount {
+	private String stockNames;
+    private int numOfShares;
+    private double sharePrices;
+    private double totalValueOfShares;
+
+    public String getStockNames() {
+        return stockNames;
+    }
+
+    public void setStockNames(String stockNames) {
+        this.stockNames = stockNames;
+    }
+
+    public int getNumOfShares() {
+        return numOfShares;
+    }
+
+    public void setNumOfShares(int numOfShares) {
+        this.numOfShares = numOfShares;
+    }
+
+    public double getSharePrices() {
+        return sharePrices;
+    }
+
+    public void setSharePrices(double sharePrices) {
+        this.sharePrices = sharePrices;
+    }
+
+    public double getTotalValueOfShares() {
+        return totalValueOfShares;
+    }
+
+    public void setTotalValueOfShares(double totalValueOfShares, double sharePrices) {
+        this.totalValueOfShares = totalValueOfShares;
+    }
+}
